b6da3de83e2596b2fab522022aafdd7b330f64c3
Liz  Shaw <lizshaw93@hotmail.com>
2019-01-06 23:08:27 +0000

    canvas radar chart drawing

--- js/chart.js
+++ js/chart.js
@@ -1,7 +1,9 @@
 var c = document.getElementById("radarChart");
 var ctx = c.getContext("2d");
 
-
+function addLabels(){
+  
+}
 function drawPentagon(){
 
     function pentagon(x, y, radius, rotation){
@@ -17,7 +19,7 @@ function drawPentagon(){
 
     ctx.beginPath();
     pentagon(180,190,180,-Math.PI / 2);
-
+    ctx.lineWidth = 2;
     ctx.stroke();
 
 };
@@ -25,6 +27,24 @@ function drawPentagon(){
 
 function drawGuidelines(){
 
+      function guideLines(){
+         var xCoords = [180, 350, 285, 75, 10];
+         var yCoords = [10, 135, 335, 335, 135];
+
+         for(var i = 0; i <= 4; i++){
+          ctx.moveTo(180,190); // center start point
+          var x = xCoords[i];
+          var y = yCoords[i];
+          ctx.lineTo(x,y);
+         }
+  }
+
+  ctx.beginPath();
+  guideLines();
+  ctx.lineWidth = 2;
+  ctx.stroke();
+
 }
 drawPentagon();
 drawGuidelines();
+addLabels();
