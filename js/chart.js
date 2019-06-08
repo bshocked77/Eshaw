02c7b070cd95b17786bda85c73c4a85f3dc196c0
Liz  Shaw <lizshaw93@hotmail.com>
2019-06-08 16:38:13 +0100

    portfolio page content creation

--- js/chart.js
+++ js/chart.js
@@ -1,9 +1,34 @@
 var c = document.getElementById("radarChart");
 var ctx = c.getContext("2d");
 
-function addLabels(){
-  
+function drawChartArea(){
+  ctx.beginPath();
+  ctx.moveTo(205, 60); // HTML point
+  ctx.lineTo(330, 165); // CSS point
+  ctx.lineTo(270, 292); // GIT point
+  ctx.lineTo(160, 270); // PHP point
+  ctx.lineTo(90, 168); // JS point
+  ctx.lineTo(205, 60); // Back to start
+
+  ctx.lineWidth = 1.6;
+  ctx.fillStyle = '#5a829a';
+  ctx.fill();
+  ctx.stroke();
+
+
 }
+
+function addLabels(){
+  ctx.font = "18px lato, sans-serif";
+  ctx.fillText("HTML", 180, 30);
+  ctx.fillText("CSS", 360, 130);
+  ctx.fillText("Javascript", 8, 120);
+  ctx.fillText("PHP", 40, 340);
+  ctx.fillText("GIT", 330, 340);
+}
+
+
+
 function drawPentagon(){
 
     function pentagon(x, y, radius, rotation){
@@ -18,7 +43,7 @@ function drawPentagon(){
     }
 
     ctx.beginPath();
-    pentagon(180,190,180,-Math.PI / 2);
+    pentagon(205,205,150,-Math.PI / 2);
     ctx.lineWidth = 2;
     ctx.stroke();
 
@@ -28,11 +53,11 @@ function drawPentagon(){
 function drawGuidelines(){
 
       function guideLines(){
-         var xCoords = [180, 350, 285, 75, 10];
-         var yCoords = [10, 135, 335, 335, 135];
+         var xCoords = [205, 348, 293, 117, 62];
+         var yCoords = [55, 159, 325, 325, 158];
 
          for(var i = 0; i <= 4; i++){
-          ctx.moveTo(180,190); // center start point
+          ctx.moveTo(210,205); // center start point
           var x = xCoords[i];
           var y = yCoords[i];
           ctx.lineTo(x,y);
@@ -46,5 +71,6 @@ function drawGuidelines(){
 
 }
 drawPentagon();
-drawGuidelines();
 addLabels();
+drawChartArea();
+drawGuidelines();
